e66423514723d74254a12f4306aa8c4ebca0117a
maheshkumar-wd <maheshkumaryadav1430@gmail.com>
2022-06-01 22:43:01 +0530

    adding youtube-1 project

---
+++ UNIT-3/JS_201/W_3/DAY_2/youtube-1/script/play.js
@@ -0,0 +1,17 @@
+let playVideo = document.getElementById("playVideo");
+let v = JSON.parse(localStorage.getItem("currVideo"));
+let play = (v) => {
+  cards.innerHTML = null; // cards is declared in index.js
+  let url = `https://www.youtube.com/embed/${v.id.videoId}`;
+  playVideo.innerHTML = `
+    <iframe
+      width="560"
+      height="315"
+      src= ${url}
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>`;
+};
+play(v);
